7aafd530b51dc1d8cc94183865f9a9baa095bd21
Ashley <aesimpson89@gmail.com>
2018-10-09 14:48:06 -0500

    added code blocks
    added back ticks to better format the SQL code blocks

--- drills1-2-5.sql
+++ drills1-2-5.sql
@@ -1,4 +1,6 @@
 -- 1) What are the three longest trips on rainy days?
+
+```SQL
 WITH raining as 
 (
 SELECT 
@@ -16,9 +18,12 @@ JOIN raining as r
 on DATE(t.start_date) = r.rain_date
 ORDER BY duration DESC
 LIMIT 3;
+```
 
 
 -- 2) Which station is full most often?
+
+```SQL
 SELECT
 	status.station_id, 
 	stations.name,
@@ -28,10 +33,13 @@ JOIN stations
 on status.station_id = stations.station_id
 GROUP BY 1,2
 ORDER BY empty_count DESC;
+```
 
 
 -- 3) Return a list of stations with a count of number of trips 
 -- 		starting at that station but ordered by dock count.
+
+```SQL
 SELECT trips.start_station as station_name, 
 	COUNT(trips.start_station) station_count, 
 	stations.dockcount 
@@ -40,10 +48,13 @@ JOIN stations
 ON trips.start_station = stations.name
 GROUP BY 1,3
 ORDER BY stations.dockcount;
+```
 
 
 -- 4) (Challenge) What's the length of the longest 
 -- 		trip for each day it rains anywhere?
+
+```SQL
 WITH rainy as 
 (
 SELECT 
@@ -68,3 +79,4 @@ SELECT
 from rain_trips
 GROUP BY 1
 ORDER BY max_duration DESC;
+```
